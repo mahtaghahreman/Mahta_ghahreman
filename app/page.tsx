73e24e22e303fc94dc2e38a608ae7d9ac5bf7956
mahtaghahreman <gh.mahta@gmail.com>
2026-07-27 08:33:49 +0330

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>مهتا قهرمان</h1>
+      <h2>Mahta Ghahreman</h2>
+      <p>Official Website</p>
+      <p>هر ترانه، تکه‌ای از یک زندگی...</p>
+    </main>
+  );
+}
