cb1e88cfbeb26d4e68393d6ae4013aaa36dbf290
mahtaghahreman <gh.mahta@gmail.com>
2026-08-04 13:22:48 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Mahta Ghahreman",
+  description: "Official Website of Mahta Ghahreman",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>{children}</body>
+    </html>
+  );
+}
