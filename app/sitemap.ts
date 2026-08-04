b261f6ba8e45d26c5354a59bd99e509fa038b49c
mahtaghahreman <gh.mahta@gmail.com>
2026-08-04 14:27:59 +0330

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,7 @@
-
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-
   return [
-
     {
       url: "https://mahta-ghahreman.vercel.app/",
       lastModified: new Date(),
@@ -12,6 +9,32 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
 
-  ];
+    {
+      url: "https://mahta-ghahreman.vercel.app/lyrics",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
 
+    {
+      url: "https://mahta-ghahreman.vercel.app/lyrics/khatereh",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+
+    {
+      url: "https://mahta-ghahreman.vercel.app/lyrics/panjereh",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+
+    {
+      url: "https://mahta-ghahreman.vercel.app/lyrics/vaghti-residam",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+  ];
 }
