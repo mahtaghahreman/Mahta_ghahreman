66b65068bf1e8c072fad32a24d37b4e376ac44e1
mahtaghahreman <gh.mahta@gmail.com>
2026-08-04 11:14:43 +0330

    Update page.tsx

--- app/lyrics/khatereh/page.tsx
+++ app/lyrics/khatereh/page.tsx
@@ -1,1 +1,28 @@
 
+import Link from "next/link";
+
+export default function LyricsPage() {
+  return (
+    <main className="lyrics-page">
+
+      <h1>اشعار</h1>
+
+      <div className="lyrics-list">
+
+        <Link href="/lyrics/khatereh">
+          خاطره
+        </Link>
+
+        <Link href="/lyrics/panjereh">
+          پنجره
+        </Link>
+
+        <Link href="/lyrics/vaghti-residam">
+          وقتی رسیدم
+        </Link>
+
+      </div>
+
+    </main>
+  );
+}
