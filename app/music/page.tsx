83a20511942b237b11205c3677c3c4af24391286
mahtaghahreman <gh.mahta@gmail.com>
2026-08-04 19:31:36 +0330

    Update page.tsx

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,1 +1,62 @@
 
+export default function MusicPage() {
+  return (
+    <main className="lyrics-page">
+
+      <h1>آهنگ‌ها</h1>
+
+      <div className="music-list">
+
+        <div className="music-card">
+
+          <h3>خاطره</h3>
+
+          <p>
+            ترانه‌سرا: مهتا قهرمان
+          </p>
+
+          <audio controls>
+            <source src="/Khatereh.mp3" type="audio/mpeg" />
+          </audio>
+
+        </div>
+
+
+        <div className="music-card">
+
+          <h3>پنجره</h3>
+
+          <p>
+            ترانه‌سرا: مهتا قهرمان
+          </p>
+
+          <audio controls>
+            <source src="/Panjereh.mp3" type="audio/mpeg" />
+          </audio>
+
+        </div>
+
+
+        <div className="music-card">
+
+          <h3>وقتی رسیدم</h3>
+
+          <p>
+            ترانه‌سرا: مهتا قهرمان
+          </p>
+
+          <p>
+            اجرا و تولید موسیقی: با استفاده از هوش مصنوعی
+          </p>
+
+          <audio controls>
+            <source src="/When-resid.mp3" type="audio/mpeg" />
+          </audio>
+
+        </div>
+
+      </div>
+
+    </main>
+  );
+}
