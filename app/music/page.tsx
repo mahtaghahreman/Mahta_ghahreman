cd152b25eb95eb401a0ed940684a5cfa5062f987
mahtaghahreman <gh.mahta@gmail.com>
2026-08-10 13:39:33 +0330

    Update page.tsx

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,4 +1,4 @@
-
+```jsx
 export default function MusicPage() {
   return (
     <main className="lyrics-page">
@@ -8,7 +8,6 @@ export default function MusicPage() {
       <div className="music-list">
 
         <div className="music-card">
-
           <h3>خاطره</h3>
 
           <p>
@@ -18,12 +17,10 @@ export default function MusicPage() {
           <audio controls>
             <source src="/Khatereh.mp3" type="audio/mpeg" />
           </audio>
-
         </div>
 
 
         <div className="music-card">
-
           <h3>پنجره</h3>
 
           <p>
@@ -33,12 +30,10 @@ export default function MusicPage() {
           <audio controls>
             <source src="/Panjereh.mp3" type="audio/mpeg" />
           </audio>
-
         </div>
 
 
         <div className="music-card">
-
           <h3>وقتی رسیدم</h3>
 
           <p>
@@ -52,7 +47,57 @@ export default function MusicPage() {
           <audio controls>
             <source src="/When-resid.mp3" type="audio/mpeg" />
           </audio>
+        </div>
 
+
+        <div className="music-card">
+          <h3>ایران فارسی</h3>
+
+          <p>
+            ترانه‌سرا: مهتا قهرمان
+          </p>
+
+          <p>
+            اجرا و تولید موسیقی: با استفاده از هوش مصنوعی
+          </p>
+
+          <audio controls>
+            <source src="/ایران.mp3" type="audio/mpeg" />
+          </audio>
+        </div>
+
+
+        <div className="music-card">
+          <h3>کوچه گرد</h3>
+
+          <p>
+            ترانه‌سرا: مهتا قهرمان
+          </p>
+
+          <p>
+            اجرا و تولید موسیقی: با استفاده از هوش مصنوعی
+          </p>
+
+          <audio controls>
+            <source src="/کوچه گرد.mp3" type="audio/mpeg" />
+          </audio>
+        </div>
+
+
+        <div className="music-card">
+          <h3>بی کران</h3>
+
+          <p>
+            ترانه‌سرا: مهتا قهرمان
+          </p>
+
+          <p>
+            اجرا و تولید موسیقی: با استفاده از هوش مصنوعی
+          </p>
+
+          <audio controls>
+            <source src="/بیکران.mp3" type="audio/mpeg" />
+          </audio>
         </div>
 
       </div>
@@ -60,3 +105,4 @@ export default function MusicPage() {
     </main>
   );
 }
+```
