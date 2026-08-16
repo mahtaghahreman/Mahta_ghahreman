e13182e16e795053f578e56c48a42c30deae4ae7
mahtaghahreman <gh.mahta@gmail.com>
2026-08-16 11:37:58 +0330

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,51 +1,77 @@
-import type { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://mahta-ghahreman.vercel.app";
+
   return [
     {
-      url: "https://mahta-ghahreman.vercel.app/",
+      url: `${baseUrl}/`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
-
     {
-      url: "https://mahta-ghahreman.vercel.app/about",
+      url: `${baseUrl}/about`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
-
     {
-      url: "https://mahta-ghahreman.vercel.app/music",
+      url: `${baseUrl}/music`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
-
     {
-      url: "https://mahta-ghahreman.vercel.app/lyrics",
+      url: `${baseUrl}/lyrics`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
-
     {
-      url: "https://mahta-ghahreman.vercel.app/lyrics/khatereh",
+      url: `${baseUrl}/lyrics/khatereh`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
-
     {
-      url: "https://mahta-ghahreman.vercel.app/lyrics/panjereh",
+      url: `${baseUrl}/lyrics/panjereh`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
-
     {
-      url: "https://mahta-ghahreman.vercel.app/lyrics/vaghti-residam",
+      url: `${baseUrl}/lyrics/vaghti-residam`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/lyrics/iran`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/lyrics/kooche-gard`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/lyrics/bikaran`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/lyrics/jan-o-del`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/lyrics/shekveh`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
