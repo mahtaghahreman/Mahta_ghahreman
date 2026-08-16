4310704057f656b54a642e7702bc33e9347d47b4
mahtaghahreman <gh.mahta@gmail.com>
2026-08-16 11:20:58 +0330

    Update page.tsx

--- app/lyrics/page.tsx
+++ app/lyrics/page.tsx
@@ -1,40 +1,43 @@
 import Link from "next/link";
 
 export default function LyricsPage() {
-return ( <main> <h1>اشعار</h1>
+  return (
+    <main>
+      <h1>اشعار</h1>
 
+      <div className="lyrics-list">
+        <Link href="/lyrics/khatereh">
+          خاطره
+        </Link>
 
-  <div className="lyrics-list">
-    <Link href="/lyrics/khatereh">
-      خاطره
-    </Link>
+        <Link href="/lyrics/panjereh">
+          پنجره
+        </Link>
 
-    <Link href="/lyrics/panjereh">
-      پنجره
-    </Link>
+        <Link href="/lyrics/vaghti-residam">
+          وقتی رسیدم
+        </Link>
 
-    <Link href="/lyrics/vaghti-residam">
-      وقتی رسیدم
-    </Link>
+        <Link href="/lyrics/iran">
+          ایران
+        </Link>
 
-    <Link href="/lyrics/iran">
-      ایران
-    </Link>
+        <Link href="/lyrics/kooche-gard">
+          کوچه گرد
+        </Link>
 
-    <Link href="/lyrics/kooche-gard">
-      کوچه گرد
-    </Link>
+        <Link href="/lyrics/bikaran">
+          بی کران
+        </Link>
 
-    <Link href="/lyrics/bikaran">
-      بی کران
-    </Link>
+        <Link href="/lyrics/jan-o-del">
+          جان و دل
+        </Link>
 
-    <Link href="/lyrics/jan-o-del">
-      جان و دل
-    </Link>
-  </div>
-</main>
-
-
-);
+        <Link href="/lyrics/shekveh">
+          شکوه
+        </Link>
+      </div>
+    </main>
+  );
 }
