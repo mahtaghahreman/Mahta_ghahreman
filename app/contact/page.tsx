d25e841b9c1213763a88e74185c8b491a7f1eaaa
mahtaghahreman <gh.mahta@gmail.com>
2026-08-23 14:04:54 +0330

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,1 +1,46 @@
 
+export default function ContactPage() {
+  return (
+    <main className="lyrics-page">
+
+      <h1>ارتباط با من</h1>
+
+      <div className="contact">
+
+        <div className="contact-box">
+          <div>
+            <h3>Instagram</h3>
+
+            <a
+              href="https://instagram.com/mahtaghahreman"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              instagram.com/mahtaghahreman
+            </a>
+          </div>
+        </div>
+
+        <div className="contact-box">
+          <div>
+            <h3>YouTube</h3>
+
+            <a
+              href="https://youtube.com/@mahta.ghahreman"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              youtube.com/@mahta.ghahreman
+            </a>
+          </div>
+        </div>
+
+      </div>
+
+      <footer>
+        © 2026 Mahta Ghahreman
+      </footer>
+
+    </main>
+  );
+}
